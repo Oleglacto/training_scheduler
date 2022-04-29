4470eb3f233271d3b149344f1ee24f36a1b6023c
Oleg Laktiushkin <o.laktiushkin@city-mobil.ru>
2022-04-29 16:45:15 +0300

    added web

---
+++ web/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <BaseHeader />
+  <div style="display: flex">
+    <BaseSide />
+    <div>
+      <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
+      <HelloWorld :msg="msg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  created() {
+    fetch('/api/v1/test').then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.msg = data.text
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: var(--el-text-color-primary);
+}
+
+.element-plus-logo {
+  width: 50%;
+}
+</style>
